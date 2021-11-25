d879a9e3bea26f6d7e4b6ef5d0927c17185195f5
angrynecromancer <95032677+angrynecromancer@users.noreply.github.com>
2021-11-25 16:02:07 +0100

    Update UserIntake.java

--- UserIntake.java
+++ UserIntake.java
@@ -1,86 +1,55 @@
+//reax1046 Rebecka Axskjöld
 import java.io.InputStream;
 import java.lang.annotation.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
-/**
- * Denna annotering används för att markera klassen du ska skriva i U6.3.
- * Det enda du behöver göra är att lägga denna java-fil i samma katalog
- * som klassen du vill märka ut, och skriva @U6_3 precis ovanför klassen.
- * Ett exempel finns i instruktionerna för uppgiften.
- * <p>
- * Detta gör att testfallen kan hitta klassen, och är nödvändigt eftersom
- * du själv bestämmer namnet på klassen till skillnad från i U6.2.
- * <p>
- * Du ska INTE lämna in denna fil. Den läggs automatiskt till i VPL.
- */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-@interface U6_3 {
-    @U6_3
-    //motivera för statiska variabler, måste använda static på array så att den är tillgänglig för alla instanser
-    public class UserIntake {
+@UnderTest(id = "U6.3")
 
-        //private InputStream userInput;
-        private Scanner input;
-        //private static InputStream[] userInputs;
-        private ArrayList<InputStream> userInputs = new ArrayList<>();
+//motivera för statiska variabler, måste använda static på array så att den är tillgänglig för alla instanser
+public class UserIntake {
 
-        //skapar en instans av denna klass i en annan klass - defaultvärde
-        public UserIntake() {
-            this(System.in); //den anropar korrekt
+    private static ArrayList<InputStream> userInputs = new ArrayList<>();
+    private Scanner input;
+    
+    //skapar en instans av denna klass i en annan klass - defaultvärde
+    public UserIntake() {
+        this(System.in); //den anropar korrekt
 
-        }
-        //jämföra kolla om userInput finns i userInputs och om inte throw stateexception ÄNDRA TILL ARRAYLIST
-        /*public UserIntake(InputStream userInput) {
-            this.input = new Scanner(userInput);
-            if(userInputs == null) { //finns userInputs deklarerat?
-                userInputs = new InputStream[1]; //deklarerar array userInputs
-                userInputs[0] = userInput; //userInput sätts på första index
-            } else {
-                for(int i = 0; i < userInputs.length; i++ ) { //om arrayen redan finns så gås den igenom i denna for-sats
-                    if(userInputs[i] == userInput) { //om strömmen som ligger på index i är samma som den nya userInput
-                        throw new IllegalStateException("Det finns redan en instans av detta, try again"); //om det är det så KASTAR vi
-                    }
-                }
-                InputStream[] tempUserInputs = new InputStream[userInputs.length + 1]; //skapa temporär array med ett längre index (+1)
-                System.arraycopy(userInputs, 0, tempUserInputs, 0, userInputs.length); //kopierar userInputs till tempUserInputs
-                userInputs = tempUserInputs; //det blir samma array fast med en extra plats
-                userInputs[userInputs.length - 1] = userInput; //lägger userInput på sista platsen
-            }
-        }*/
+    }
 
-        public UserIntake(InputStream userInput) {
-            this.input = new Scanner(userInput);
-            if (userInputs == null) {
-                
-            }
-        }
+    public UserIntake(InputStream userInput) {
+        if (userInputs.contains(userInput)) {
+            throw new IllegalStateException("Instans av InputStream finns redan");
+        } 
+            input = new Scanner(userInput);
+            userInputs.add(userInput);
+    }
 
 
-        //lägg till lite krav på om inputen är tom eller minus eller nåt
-        //börja med stor bokstav, ta bort mellanslag osv.
+    //lägg till lite krav på om inputen är tom eller minus eller nåt
+    //börja med stor bokstav, ta bort mellanslag osv.
 
-        public void inputInt() {
-            System.out.println("> ");
-            int number = input.nextInt();
-        }
+    public int inputInt(String number) {
+        System.out.println(number + writeSign());
+        int inputNumber = input.nextInt();
+        input.nextLine();//för att rensa inputen
+        return inputNumber;
+    }
 
-        public void inputDouble() {
-            System.out.println("> ");
-            double decimal = input.nextDouble();
-        }
+    public double inputDouble(String decimal) {
+        System.out.println(decimal + writeSign());
+        double inputDecimal = input.nextDouble();
+        input.nextLine();//för att rensa inputen
+        return inputDecimal;
+    }
 
-        public void inputString() {
-            System.out.println("> ");
-            String word = input.nextLine();
-        }
+    public String inputString(String word) {
+        System.out.println(word + writeSign());
+        return input.nextLine();
+    }
+
+    private String writeSign() {
+        return "?>";
     }
 }
-
-
-
-
-
