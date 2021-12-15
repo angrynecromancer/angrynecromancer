ce6440184f3eab102a73b8946b94928f1403b38e
angrynecromancer <95032677+angrynecromancer@users.noreply.github.com>
2021-12-15 19:29:22 +0100

    Add files via upload

---
+++ AssignmentSevenPointSix.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AssignmentSevenPointSix {
+    //@UnderTest(id = "dogs")
+    private static ArrayList<Dog> dogs = new ArrayList<>();
+
+    //@UnderTest(id = "U7.6.1.1.") Egen metod för att byta plats
+    public void swapPlaces(int i, int j){
+        for ()
+    }
+
+    //@UnderTest(id = "U7.6.1.2.") Använda en metod från klass biblioteket
+    public static void swap(int i, int j) {
+        Collections.swap(dogs, i, j);
+    }
+
+    //skriv 3 metoder, en som jämför svanslängd, en som jämför namn och en som jämför både namn och svanslängd
+    //@UnderTest(id = "U7.6.2") Jämföra två hundar
+    public void compareDogs() {
+
+    }
+
+    //@UnderTest(id = "U7.6.3") Hitta den minsta hunden
+    public void findSmallestDog() {
+
+    }
+
+    //@UnderTest(id = "U7.6.4") Implementera sorteringsalgoritmen
+    public void sort() {
+
+    }
+}
